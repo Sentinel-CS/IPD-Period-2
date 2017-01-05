b548415490eeab9a7442ca3e26511947630f6db3
Nash Sauter <nksaut0420@student.mcpsmt.org>
2017-01-05 08:35:56 -0700

    Update team6.py
    Added Team 6 Strategy

--- team6.py
+++ team6.py
@@ -6,29 +6,33 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'team_name' # Only 10 chars displayed.
+strategy_name = 'Strategy Identification'
+strategy_description = 'It tries to define the opponents strategy and react accordingly.'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+   # If the other player has betrayed or this is the last half of the game, 
+    if len(my_history) < 6: 
+        return 'c'
+        return 'b'
+        return 'b'
+        return 'c'
+        return 'c'
+        return 'b'
+    else:
+        if 'cbbcc' in their_history:
+            if their_history[-1]=='b' and their_history[-2]=='b':
+                return 'b'
+            else:
+                return 'c'
+        elif 'cccccc' in their_history:
+            return 'b'
+        elif 'bbbbb' in their_history:
+            return 'b'
+        else:
+            return 'b'
+            
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
