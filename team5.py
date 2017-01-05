ad63d765d384698e22eafc83bfbd1dfd4cb1ef87
Cory Emlen <cemle0418@student.mcpsmt.org>
2017-01-05 08:36:13 -0700

    Added strategy for team 5

--- team5.py
+++ team5.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +8,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = '|e^iπ|' # Only 10 chars displayed.
+strategy_name = 'Tit for Tat with forgiveness and defense'
+strategy_description = 'Our program returns what our opponent returns, with a 2% chance to give a c if we were betrayed to break up infinite b loops.  If this doesnt work then it returns b.  It also returns b if its opponent is alternating, as this is a better strategy against that.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +19,22 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(my_history) == 0:
+        return 'c'
+    if their_history[-1] == 'c':
+        if their_history[-4:] == 'bcbc':
+            return 'b'
+        return 'c'
+    elif their_history[-1] == 'b':
+        if their_history[-4:] == 'cbcb':
+            return 'b'
+        if random.randint(0,50) == 42:
+            if 'c' in my_history[-10:] and 'c' not in their_history[-10:]:
+                return 'b'
+            return 'c'
+        return 'b'
+    else:
+        return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +43,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
